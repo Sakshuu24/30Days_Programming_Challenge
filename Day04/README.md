6e69267cfb182858dcdd84f59c6c6e9cf2884852
sakshuu24 <sakshipingal058@gmail.com>
2025-08-18 19:47:11 +0530

    Added Day04: Merge Two Sorted Arrays

---
+++ Day04/README.md
@@ -0,0 +1,9 @@
+# Day 04 - Merge Two Sorted Arrays
+
+### Problem Statement:
+You are given two sorted arrays `arr1` of size `m` and `arr2` of size `n`.  
+Your task is to merge these two arrays into a single sorted array **without using extra space**.
+
+---
+
+
---
+++ Day04/problem.py
@@ -0,0 +1,15 @@
+"""
+Problem: Merge Two Sorted Arrays
+
+You are given two sorted arrays arr1 of size m and arr2 of size n. 
+Your task is to merge these two arrays into a single sorted array 
+without using any extra space (i.e., in-place merging). 
+
+The elements in arr1 should be merged first, followed by the elements of arr2, 
+resulting in both arrays being sorted after the merge.
+
+Example:
+Input: arr1 = [1, 3, 5, 7], arr2 = [2, 4, 6, 8]
+Output: arr1 = [1, 2, 3, 4], arr2 = [5, 6, 7, 8]
+"""
+
