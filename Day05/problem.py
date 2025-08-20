2ee4a72a82d6fc1e712e200a861a20ee70e231c5
sakshuu24 <sakshipingal058@gmail.com>
2025-08-20 16:58:45 +0530

    Added Day05:Leaders in an array

---
+++ Day05/problem.py
@@ -0,0 +1,18 @@
+"""
+Problem: Find the Leaders in an Array
+
+You are given an integer array arr of size n. 
+An element is considered a leader if it is greater than all the elements to its right. 
+Your task is to find all such leader elements in the array.
+
+Input:
+- An integer array arr of size n.
+
+Output:
+- Return an array containing all the leader elements in the order 
+  in which they appear in the original array.
+
+Example:
+Input: [16, 17, 4, 3, 5, 2]
+Output: [17, 5, 2]
+"""
